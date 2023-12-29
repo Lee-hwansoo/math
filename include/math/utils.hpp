e99e587b1a979339127feeae4e7b0fb39c0fd5b1
Lee-hwansoo <lee_hwansoo@naver.com>
2023-12-29 20:20:43 +0000

    Added skeleton codes for math utils

---
+++ include/math/utils.hpp
@@ -0,0 +1,18 @@
+/**
+ * @file utils.hpp
+ * @author HwanSoo Lee
+ * @brief header of math.
+ * @details This file contains utils for math.
+ * @version 1.0.0
+ * @date 2023-12-30
+ * @copyright Copyright (c) 2023
+ */
+
+#ifndef LEEHWANSOO_MATH_UTILS_HPP_
+#define LEEHWANSOO_MATH_UTILS_HPP_
+
+namespace leehwansoo::math{
+
+} // LEEHWANSOO_MATH_UTILS_HPP_
+
+#endif
